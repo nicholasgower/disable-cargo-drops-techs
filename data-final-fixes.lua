021469e246ec636743220dc1157d728274b92746
Nicholas Gower <nicholasgower01@gmail.com>
2025-01-21 22:31:14 -0800

    compat: Added support for hardcore Fulgora, Gleba, and Vulcanus.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,4 +3,8 @@ for _,tech in pairs(data.raw["technology"]) do
         tech = nil
     end
 end
-data.raw["technology"]["cerys-cargo-drops"] = nil
+data.raw["technology"]["planetslib-cerys-cargo-drops"] = nil
+data.raw["technology"]["planetslib-fulgora-cargo-drops"] = nil
+data.raw["technology"]["planetslib-gleba-cargo-drops"] = nil
+data.raw["technology"]["planetslib-vulcanus-cargo-drops"] = nil
+--data.raw["technology"]["cerys-cargo-drops"] = nil
